4f0027905986de90aab7a8448a81a17b88d71191
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-07-31 16:44:39 -0400

    add prod files

---
+++ src/Operadores.java
@@ -0,0 +1,26 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "Artur" + "Vasconcelos";
+
+        System.out.println(nomeCompleto);
+
+        //qual o resultado das expressoes abaixo?
+        String concatenacao ="?";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+    }
+}
