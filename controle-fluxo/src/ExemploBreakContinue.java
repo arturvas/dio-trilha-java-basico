b97781e3add7abe8cdb80ae986c3b1c77677c677
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-08-04 10:30:38 -0400

    edu: break & continue exemples

---
+++ controle-fluxo/src/ExemploBreakContinue.java
@@ -0,0 +1,9 @@
+public class ExemploBreakContinue  {
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i++) {
+            if (i == 3)
+                break;
+            System.out.println(i);
+        }
+    }
+}
