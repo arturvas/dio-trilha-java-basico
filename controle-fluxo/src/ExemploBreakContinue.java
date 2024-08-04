2541eb6ec34383994053991e83fb076a0f542808
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-08-04 10:32:57 -0400

    refactor: add comments

--- controle-fluxo/src/ExemploBreakContinue.java
+++ controle-fluxo/src/ExemploBreakContinue.java
@@ -2,7 +2,8 @@ public class ExemploBreakContinue  {
     public static void main(String[] args) {
         for (int i = 0; i < 6; i++) {
             if (i == 3)
-                break;
+                break; //para a verificacao.
+                //continue; pula a verificacao ao chegar no contador 3.
             System.out.println(i);
         }
     }
